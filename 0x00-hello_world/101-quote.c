3af3cd8401b5b1643b4607bb294418cf494ec661
Meti-Adugna <metiadugna14@gmail.com>
2022-06-03 21:12:03 -0700

    commit 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 1
+ */
+int main(void)
+{
+char *s="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+while (*s) putchar(*s++);
+}
